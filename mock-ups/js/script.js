3708be7390895512bad1638f4e03112ff48a111f
Łukasz Ustach <lukasz.ustach@gmail.com>
2018-04-22 13:31:42 +0200

    add new view and begin build function in js

---
+++ mock-ups/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",	function	()	{
+
+var checkbox=document.querySelectorAll('input');
+console.log(checkbox);
+var userInfo=["add your note","select checkbox","correct checkbox"];
+
+function checkbox() {
+    
+
+}
+
+});
---
+++ src/js/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded',
+function(){
+    class Header extends React.Component{
+        render(){
+            return
+        }
+    }
+    class ToDoList extends React.Component{
+        render(){
+            return
+        }
+    }
+    class ToDoItem extends React.Component{
+        render(){
+            return
+        }
+    }
+    class NewToDo extends React.Component{
+        render(){
+            return
+        }
+    }
+    class App extends React.Component{
+        render(){
+            return <h1>hello hi!</h1>;
+        }
+    }
+		ReactDOM.render(
+				<App />,
+				document.getElementById('app')
+		);
+});
